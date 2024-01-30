451ab409d5e53e32b1517a2a0234858ad20ad4a0
Nikola Kolev <nickkolev06@abv.bg>
2024-01-30 14:56:21 +0200

    Initial setup +header and footer separate files

---
+++ wp-content/themes/softuni-exam/header.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Page Title -->
+    <title>Foodfun</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="https://localhost/suexam/wp-content/themes/softuni-exam/assets/images/logo/favicon.png" type="image/x-icon">
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="https://localhost/suexam/wp-content/themes/softuni-exam/assets/css/animate-3.7.0.css">
+    <link rel="stylesheet" href="https://localhost/suexam/wp-content/themes/softuni-exam/assets/css/font-awesome-4.7.0.min.css">
+    <link rel="stylesheet" href="https://localhost/suexam/wp-content/themes/softuni-exam/assets/css/bootstrap-4.1.3.min.css">
+    <link rel="stylesheet" href="https://localhost/suexam/wp-content/themes/softuni-exam/assets/css/owl-carousel.min.css">
+    <link rel="stylesheet" href="https://localhost/suexam/wp-content/themes/softuni-exam/assets/css/jquery.datetimepicker.min.css">
+    <link rel="stylesheet" href="https://localhost/suexam/wp-content/themes/softuni-exam/assets/css/style.css">
+</head>
+<body>
+    <!-- Preloader Starts -->
+    <div class="preloader">
+        <div class="spinner"></div>
+    </div>
+    <!-- Preloader End -->
+
+    <!-- Header Area Starts -->
+	<header class="header-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2">
+                    <div class="logo-area">
+                        <a href="index.html"><img src="https://localhost/suexam/wp-content/themes/softuni-exam/assets/images/logo/logo.png" alt="logo"></a>
+                    </div>
+                </div>
+                <div class="col-lg-10">
+                    <div class="custom-navbar">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>  
+                    <div class="main-menu">
+                        <ul>
+                            <li class="active"><a href="index.html">home</a></li>
+                            <li><a href="about.html">about</a></li>
+                            <li><a href="menu.html">menu</a></li>
+                            <li><a href="#">blog</a>
+                                <ul class="sub-menu">
+                                    <li><a href="blog-home.html">Blog Home</a></li>
+                                    <li><a href="blog-details.html">Blog Details</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="contact-us.html">contact</a></li>
+                            <li><a href="elements.html">Elements</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header Area End -->
+
+    <!-- Banner Area Starts -->
+    <section class="banner-area text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h6>the most interesting food in the world</h6>
+                    <h1>Discover the <span class="prime-color">flavors</span><br>  
+                    <span class="style-change">of <span class="prime-color">food</span>fun</span></h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Banner Area End -->
